37e0b25c8dde73bf2d39e96e134fd3de92016be1
fsafdsa Lfybkf <P.danila228@mail.ru>
2020-12-18 18:28:31 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2212121/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2212121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int a, b, c;
+                Console.Write("Enter a: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter b: ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter c: ");
+                c = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+                var min = new[] { a, b, c }.Min();
+
+
+                Console.WriteLine("min " + min);
+
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
